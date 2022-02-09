cbce84add8cfa52106638896fa24905fb1ed5ded
StrangePerch <sasha.aav.sv@gmail.com>
2022-02-09 20:24:22 +0400

    Заменил string на string builder ради оптимизации

--- src/main/java/igorlink/command/DonateSubCommand.java
+++ src/main/java/igorlink/command/DonateSubCommand.java
@@ -6,38 +6,40 @@ import org.bukkit.command.CommandSender;
 
 public class DonateSubCommand {
     public static void onDonateCommand(CommandSender sender, String[] args) {
+
+        //Getting donation's amount
+        String donationAmount = args[0];
+        StringBuilder donationUsernameBuilder = new StringBuilder();
+        StringBuilder donationMessageBuilder = new StringBuilder();
+
         int i;
-
-        //Getting donation's amount
-        String donationAmount = new String();
-        String donationUsername = new String();
-        String donationMessage = new String();
-
-        //Getting donation's amount
-        donationAmount = args[0];
-
         //Получаем имя донатера
         for (i = 1; i <= args.length - 1; i++) {
             if (args[i].equals("##")) {
                 break;
-            }
-            else {
-                if (i==1) {
-                    donationUsername = donationUsername + args[i];
-                }
-                else {
-                    donationUsername = ' ' + donationUsername + args[i];
+            } else {
+                if (i == 1) {
+                    donationUsernameBuilder.append(args[i]);
+                } else {
+                    donationUsernameBuilder.append(" ").append(args[i]);
                 }
             }
         }
 
         //Все, что после символов ## - это сообщение
-        for (i = i+1; i <= args.length - 1; i++)
-        {
-            donationMessage = donationMessage + args[i] + ' ';
+        for (i++; i <= args.length - 1; i++) {
+            donationMessageBuilder.append(args[i]).append(' ');
         }
 
+        String donationUsername = donationUsernameBuilder.toString();
+        String donationMessage = donationMessageBuilder.toString();
+
         //Отправляем донат на исполнение
-        DonationExecutor.getInstance().streamerPlayersManager.addToDonationsQueue(new Donation(sender, donationUsername, donationAmount+".00", donationMessage));
+        DonationExecutor.getInstance().streamerPlayersManager.addToDonationsQueue(
+                new Donation(
+                        sender,
+                        donationUsername,
+                        donationAmount + ".00",
+                        donationMessage));
     }
 }
