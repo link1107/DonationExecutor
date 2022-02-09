f67e80b58422575c7f69a3ccc5a48a80bfebf688
StrangePerch <sasha.aav.sv@gmail.com>
2022-02-09 20:27:41 +0400

    Заменил кучу if'ов на switch и добавил log в случае не известной команды

--- src/main/java/igorlink/command/DonationExecutorCommand.java
+++ src/main/java/igorlink/command/DonationExecutorCommand.java
@@ -1,6 +1,7 @@
 package igorlink.command;
 
 import org.bukkit.command.CommandSender;
+
 import static igorlink.service.Utils.logToConsole;
 
 public class DonationExecutorCommand extends AbstractCommand {
@@ -18,46 +19,50 @@ public class DonationExecutorCommand extends AbstractCommand {
         }
 
         try {
-
             //Если команда - это reload, где не должно быть доп аргументов, то вызываем функцию релоуда конфига
-            if (args[0].equals("reload")) {
-                if (args.length == 1) {
-                    ReloadSubCommand.onReloadCommand(sender);
-                    return true;
-                }
-            } else if (args[0].equals("donate")) {
-                //Инициализируем список аргментов для новой сабфункции
-                //Если команда - donate, где нужен минимум 1 доп аргумент, создаем новый массив аргументов со смещением 1, и вызываем функцию обработки доната
-                if (args.length >= 2) {
-                    //Инициализируем список новых аргументов для субкоманды
-                    newArgs = new String[args.length - 1];
-                    //Создаем новый список аргументов, копируя старый со смещением 1
-                    System.arraycopy(args, 1, newArgs, 0, args.length - 1);
-                    //Вызываем обработку доната
-                    DonateSubCommand.onDonateCommand(sender, newArgs);
-                    //Возвращаем true, к все прошло успешно
-                    return true;
-                }
-            } else if (args[0].equals("filter")) {
-                if ((args.length == 2) && (args[1].equals("on")) || (args[1].equals("off"))) {
-                    //Инициализируем список новых аргументов для субкоманды
-                    newArgs = new String[args.length - 1];
-                    //Создаем новый список аргументов, копируя старый со смещением 1
-                    System.arraycopy(args, 1, newArgs, 0, args.length - 1);
-                    //Вызываем обработку доната
-                    FilterSubCommand.onFilterCommand(sender, newArgs);
-                    //Возвращаем true, к все прошло успешно
-                    return true;
-                }
+            switch (args[0]) {
+                case "reload":
+                    if (args.length == 1) {
+                        ReloadSubCommand.onReloadCommand(sender);
+                        return true;
+                    }
+                    break;
+                case "donate":
+                    //Инициализируем список аргументов для новой сабфункции
+                    //Если команда - donate, где нужен минимум 1 доп аргумент, создаем новый массив аргументов со смещением 1, и вызываем функцию обработки доната
+                    if (args.length >= 2) {
+                        //Инициализируем список новых аргументов для субкоманды
+                        newArgs = new String[args.length - 1];
+                        //Создаем новый список аргументов, копируя старый со смещением 1
+                        System.arraycopy(args, 1, newArgs, 0, args.length - 1);
+                        //Вызываем обработку доната
+                        DonateSubCommand.onDonateCommand(sender, newArgs);
+                        //Возвращаем true, к все прошло успешно
+                        return true;
+                    }
+                    break;
+                case "filter":
+                    if ((args.length == 2) && (args[1].equals("on")) || (args[1].equals("off"))) {
+                        //Инициализируем список новых аргументов для субкоманды
+                        newArgs = new String[args.length - 1];
+                        //Создаем новый список аргументов, копируя старый со смещением 1
+                        System.arraycopy(args, 1, newArgs, 0, args.length - 1);
+                        //Вызываем обработку доната
+                        FilterSubCommand.onFilterCommand(sender, newArgs);
+                        //Возвращаем true, к все прошло успешно
+                        return true;
+                    }
+                    break;
+                default:
+                    logToConsole("Неизвестная команда: " + args[0]);
+                    return false;
             }
 
         } catch (Exception e) {
-
             //Если получили exception, сообщаем о нем и выдаем сообщение об ошибке в консоль
             e.printStackTrace();
             logToConsole("Произошла неизвестная ошибка при выполнении команды!");
             return false;
-
         }
 
         //Если ничего не выполнилось - в команде была ошибка
