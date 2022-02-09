97335d2af95d44623401a2c2fdac9b18ab900b03
StrangePerch <sasha.aav.sv@gmail.com>
2022-02-09 20:29:54 +0400

    Заменил toLowerCase().equals на equalsIgnoreCase И убрал лишний cast из Sender в Player

--- src/main/java/igorlink/command/FilterSubCommand.java
+++ src/main/java/igorlink/command/FilterSubCommand.java
@@ -8,17 +8,17 @@ import org.bukkit.entity.Player;
 
 public class FilterSubCommand {
     public static void onFilterCommand(CommandSender sender, String[] args) {
-        if (args[0].toLowerCase().equals("on")) {
+        if (args[0].equalsIgnoreCase("on")) {
             MainConfig.turnFilterOn();
             Utils.logToConsole("Фильтр никнеймов донатеров §bВКЛЮЧЕН");
-            if (sender instanceof Player) {
-                Utils.sendSysMsgToPlayer((Player) sender, "Фильтр никнеймов донатеров §bВКЛЮЧЕН");
+            if (sender instanceof Player player) {
+                Utils.sendSysMsgToPlayer(player, "Фильтр никнеймов донатеров §bВКЛЮЧЕН");
             }
         } else {
             MainConfig.turnFilterOff();
             Utils.logToConsole("Фильтр никнеймов донатеров §bВЫКЛЮЧЕН");
-            if (sender instanceof Player) {
-                Utils.sendSysMsgToPlayer((Player) sender,"Фильтр никнеймов донатеров §bВЫКЛЮЧЕН");
+            if (sender instanceof Player player) {
+                Utils.sendSysMsgToPlayer(player,"Фильтр никнеймов донатеров §bВЫКЛЮЧЕН");
             }
         }
 
